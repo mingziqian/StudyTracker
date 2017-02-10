5066823b13bd5d946fbfead4dc5164da6e564042
brianliu27 <bliu27@dhcp-10-105-156-76.wireless.northwestern.private>
2017-02-10 14:51:36 -0600

    Linked home page with timer

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){ 
 
-	$('#study').click(function(){
-		$('.form, .exit').addClass('active');
+	$('.study').click(function(){
+		$('.form, .exit, .overlay').addClass('active');
 		$('#select-class').children('.content-wrapper').addClass('active');
 		$('#assignment-buttons').addClass('visible');
 	});
@@ -10,6 +10,7 @@ $(document).ready(function(){
 		if ($(this).hasClass('active')) {
 			$('.active').removeClass('active');
 			$('.visible').removeClass('visible');
+			$('.done').removeClass('done');
 		}
 	});
 	
